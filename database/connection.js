664fc2a00e5d138b82c356e152d550735927713d
adithyan-sivaraman <124238686+adithyan-sivaraman@users.noreply.github.com>
2023-08-29 13:06:10 +0530

    changes in connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import 'dotenv/config'
 
 //required for local mongodb
-const dbname = process.env.MONGO_DB
+const dbName = process.env.MONGO_DB
 const localDBUrl = process.env.MONGO_URL;
 
 //required for cloud mongodb
@@ -12,11 +12,11 @@ const  password =  process.env.MONGO_PASSWORD;
 const clusterName = process.env.MONGO_CLUSTER || '';
 
 
-const cloudUrl = `mongodb+srv://${username}:${password}@${clusterName}/${dbname}?retryWrites=true&w=majority`;
-const localUrl = `${localDBUrl}/${dbname}`;
+const cloudMongoUrl = `mongodb+srv://${username}:${password}@${clusterName}/${dbName}?retryWrites=true&w=majority`;
+const localUrl = `${localDBUrl}/${dbName}`;
 const connect = async ()=>{
 try{
-await mongoose.connect(cloudUrl,{
+await mongoose.connect(cloudMongoUrl,{
     useNewUrlParser: true,
 });
 console.log('connection established');
